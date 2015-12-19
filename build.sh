fe412d30071ef15f2dde29b17c7a625a21a73704
Rene Richter <Richterrettich@gmail.com>
2015-12-19 10:30:50 +0100

    Move build logic to build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+set -ev
 docker build -t openservice/acl-database:latest .
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/acl-database" ] ; then
+  docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" -e="$DOCKER_EMAIL"
+  docker push openservice/acl-database:latest
+fi
